5c722415b0f8d9469e65323a2f74e580525dba64
JoaquinHartwig <yanelian29@gmail.com>
2026-06-11 23:27:35 -0300

    Update Program.cs

--- Turn Based Combat Game/Turn Based Combat Game/Program.cs
+++ Turn Based Combat Game/Turn Based Combat Game/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Unit player = new Unit(100,20,12,"Pepe");
         }
     }
 }
