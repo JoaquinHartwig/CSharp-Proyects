300c349301e1110065620fe41cf952b709498be7
JoaquinHartwig <yanelian29@gmail.com>
2026-06-11 23:26:52 -0300

    Cuando una unidad nace, empieza con toda la vida.

--- Turn Based Combat Game/Turn Based Combat Game/Unit.cs
+++ Turn Based Combat Game/Turn Based Combat Game/Unit.cs
@@ -6,5 +6,35 @@ namespace Turn_Based_Combat_Game
 {
     internal class Unit
     {
+        private int _currentHP;
+        private int _maxHP;
+        private int _attackPower;
+        private int _healPower;
+        private string _nameUnit;
+        private Random _random;
+
+        public Unit(int maxHP,int attackPower,int healPower, string nameUnit)
+        {
+            _currentHP = maxHP; //Cuando una unidad nace, empieza con toda la vida.
+            _maxHP = maxHP;
+            _attackPower = attackPower;
+            _healPower = healPower;
+            _nameUnit = nameUnit;
+            _random= new Random();
+        }
+        public void Attack(Unit unittoAttack)
+        {
+            double rng = _random.NextDouble();//Genera un número aleatorio entre 0.0 y 1.0 Nunca llega a 1  
+            rng = rng / 2 + 0.75f;
+            int RandDamage = (int)(_attackPower * rng);
+            unittoAttack.TakeDamage(RandDamage);//ahora puede atacar e infligir daño
+            Console.WriteLine(_nameUnit + "attacks ", unittoAttack._nameUnit + "and deals" + RandDamage + "damage!");
+
+        }
+        public void TakeDamage(int Damage)
+        {
+            _currentHP -= Damage;
+
+        }
     }
 }
