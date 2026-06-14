1b9393cabae7aa2bf8e02aaab03c596837bf65f4
JoaquinHartwig <yanelian29@gmail.com>
2026-06-13 23:09:04 -0300

    Hoy  termino tic tac toe en consola

--- Tic Tac Toe/Tic Tac Toe/GameResultcs.cs
+++ Tic Tac Toe/Tic Tac Toe/GameResultcs.cs
@@ -7,6 +7,6 @@ namespace Tic_Tac_Toe
     public class GameResultcs
     {
         public Player Winner { get; set; }
-        public WinInfo winInfo { get; set; }
+        public WinInfo WinInfo { get; set; }
     }
 }
