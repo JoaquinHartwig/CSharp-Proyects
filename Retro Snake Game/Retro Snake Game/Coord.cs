ef77f91da7a3d33b972c4a4462a2b9ae794816dc
JoaquinHartwig <yanelian29@gmail.com>
2026-06-15 02:56:39 -0300

    s

--- Retro Snake Game/Retro Snake Game/Coord.cs
+++ Retro Snake Game/Retro Snake Game/Coord.cs
@@ -4,7 +4,21 @@ using System.Text;
 
 namespace Retro_Snake_Game
 {
-    internal class Coord
+    internal class Coord // Representa una coordenada
     {
+        private int x; //Estos atributos privados guardan los valores reales
+        private int y;
+
+        public int X { get { return x; } } // Las propeidades permite leerlos desde afuera
+        public int Y { get { return y; } }
+
+        public Coord (int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    
+       }
+
     }
-}
+
--- Retro Snake Game/Retro Snake Game/Program.cs
+++ Retro Snake Game/Retro Snake Game/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Coord gridDimension = new Coord(50, 20);
+            Coord snakePos = new Coord(10, 5);
+
+            for (int y = 0; y < gridDimension.Y; y++)
+            {
+                for (int x = 0; x < gridDimension.X; x++)
+
+                {
+                    Coord currentCoord = new Coord(x, y);
+                    if (x == snakePos.X &&
+                                              y == snakePos.Y)
+                    {
+                        Console.Write("■");
+                    }
+                    else if (x == 0 || y == 0 || x == gridDimension.X - 1 || y == gridDimension.Y - 1)
+                    {
+                        Console.Write("#");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                  
+                }
+                Console.WriteLine();
+
+
+            }
         }
     }
 }
